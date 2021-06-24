afaa7f16f71332cf23afce1e292b660e1f94ca19
constantince <alberteinstein007@126.com>
2021-06-24 16:51:50 +0800

    add new face

--- first-shader.glsl
+++ first-shader.glsl
@@ -4,18 +4,47 @@ precision mediump float;
 
 uniform vec2 u_resolution;
 uniform float u_time;
+uniform vec2 u_mouse;
 
-vec3 colorA = vec3(0.149,0.141,0.912);
-vec3 colorB = vec3(1.000,0.833,0.224);
+/* Color palette */
+#define BLACK           vec3(0.0, 0.0, 0.0)
+#define WHITE           vec3(1.0, 1.0, 1.0)
+#define RED             vec3(1.0, 0.0, 0.0)
+#define GREEN           vec3(0.0, 1.0, 0.0)
+#define BLUE            vec3(0.0, 0.0, 1.0)
+#define YELLOW          vec3(1.0, 1.0, 0.0)
+#define CYAN            vec3(0.0, 1.0, 1.0)
+#define MAGENTA         vec3(1.0, 0.0, 1.0)
+#define ORANGE          vec3(1.0, 0.5, 0.0)
+#define PURPLE          vec3(1.0, 0.0, 0.5)
+#define LIME            vec3(0.5, 1.0, 0.0)
+#define ACQUA           vec3(0.0, 1.0, 0.5)
+#define VIOLET          vec3(0.5, 0.0, 1.0)
+#define AZUR            vec3(0.0, 0.5, 1.0)
+
+float Circle(vec2 uv, float radius, vec2 p, float blur) {
+    uv -= p;               // -0.5 <> 0.5
+    uv.x *= u_resolution.x / u_resolution.y;
+    float d = length(uv);
+    float pct = smoothstep(radius, radius-blur, d);
+    return pct;
+}
 
 void main() {
-    vec3 color = vec3(0.0);
+    
+    vec2 uv = gl_FragCoord.xy / u_resolution.xy; // 0 <> 1
+   
+    float mask = Circle(uv, .1, vec2(.5), .005); // create face;
+    mask -= Circle(uv, .01, vec2(.4, .52), .005); // add new eyes;
+    mask -= Circle(uv, .01, vec2(.6, .52), .005);
 
-    float pct = abs(sin(u_time));
+    float mouth = Circle(uv, .05, vec2(.5, .47), 0.01);
+    mouth -= Circle(uv, .05, vec2(.5, .48), 0.01); 
 
-    // Mix uses pct (a value from 0-1) to
-    // mix the two colors
-    color = mix(colorA, colorB, pct);
+    mask -= mouth;
 
-    gl_FragColor = vec4(color,1.0);
+    vec3 color = YELLOW;
+    color *= mask;
+    // vec3 color = vec3(mask);
+    gl_FragColor = vec4(color, 1.0);
 }
